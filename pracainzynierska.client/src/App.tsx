53fa9bdf3725945624763e55f6f4529ae1ea8dcb
AcePeQ <maciej.nojszewski22@gmail.com>
2025-04-08 13:21:40 +0200

    feat: create fishpage view with table

--- pracainzynierska.client/src/App.tsx
+++ pracainzynierska.client/src/App.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import LoaderFull from "./components/Loaders/LoaderFull/LoaderFull";
+import FishPage from "./pages/FishPage/FishPage";
 
 const Layout = lazy(() => import("./layouts/Layout/Layout"));
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route index element={<HomePage />} />
+            <Route path="/fish" element={<FishPage />} />
           </Route>
         </Routes>
       </Suspense>
