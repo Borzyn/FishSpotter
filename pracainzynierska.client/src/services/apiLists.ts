d514cda0c9d0acab8f220b13f3279fdd6759896e
damian <damian.dziedzic.xvi@gmail.com>
2025-04-17 14:58:43 +0200

    fix

--- pracainzynierska.client/src/services/apiLists.ts
+++ pracainzynierska.client/src/services/apiLists.ts
@@ -6,8 +6,10 @@ export async function getListMapsApi() {
       },
       method: "GET",
     });
-
-    if (!res.ok) {
+      console.log(res);
+      if (!res.ok) {
+          const error = await res.json();
+          console.log(error);
       throw new Error("Fetching maps went wrong!");
     }
 
