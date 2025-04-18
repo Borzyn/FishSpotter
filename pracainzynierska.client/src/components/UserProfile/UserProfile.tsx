def9984f5ab93e349b356bb0f7b1f8e63d3649ea
AcePeQ <maciej.nojszewski22@gmail.com>
2025-04-18 13:46:08 +0200

    feat: add useMediaQuery and lucid-icons library, add mobile header and styles

--- pracainzynierska.client/src/components/UserProfile/UserProfile.tsx
+++ pracainzynierska.client/src/components/UserProfile/UserProfile.tsx
@@ -1,7 +1,15 @@
-function UserProfile() {
+import { LogOut } from "lucide-react";
+
+function UserProfile({ username }: { username: string }) {
   return (
-    <div>
-      <button></button>
+    <div className="flex gap-3 items-center">
+      <button className="text-white w-full h-full block text-lg bg-sky-500 px-2.5 py-1 rounded-md text-nowrap cursor-pointer">
+        {username}
+      </button>
+
+      <button className="cursor-pointer text-sky-500">
+        <LogOut size={32} />
+      </button>
     </div>
   );
 }
