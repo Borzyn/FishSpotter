54095287b13f8f6536cbb02297ab4896d7fbf29c
AcePeQ <maciej.nojszewski22@gmail.com>
2025-04-22 12:26:36 +0200

    feat: navigate to user profile

--- pracainzynierska.client/src/components/UserProfile/UserProfile.tsx
+++ pracainzynierska.client/src/components/UserProfile/UserProfile.tsx
@@ -1,7 +1,9 @@
 import { LogOut } from "lucide-react";
 import { useLogout } from "../../features/Authentication/useLogout";
+import { useNavigate } from "react-router";
 
 function UserProfile({ username }: { username: string }) {
+  const navigate = useNavigate();
   const { logoutFromAccount, isLoggingOut } = useLogout();
 
   function handleLogout() {
@@ -11,6 +13,7 @@ function UserProfile({ username }: { username: string }) {
   return (
     <div className="flex gap-3 items-center">
       <button
+        onClick={() => navigate(`/profile/${username}`)}
         disabled={isLoggingOut}
         className="text-white w-full h-full block text-lg bg-sky-500 px-2.5 py-1 rounded-md text-nowrap cursor-pointer"
       >
