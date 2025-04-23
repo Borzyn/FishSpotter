1b4e7c3fa7f0c2ea1a83b8d65adca7e5c8bf05d0
damian <damian.dziedzic.xvi@gmail.com>
2025-04-23 12:50:40 +0200

    d

---
+++ PracaInzynierska.Server/Models/AdditionalModels/FishAndMapModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FishSpotter.Server.Models.AdditionalModels
+{
+    public class FishAndMapModel
+    {
+        public string fishname {  get; set; }
+        public string mapname { get; set; }
+    }
+}
